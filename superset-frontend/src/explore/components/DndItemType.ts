94df753bcf4c58685f5ab47944a87a900e3e0693
Kamil Gabryjelski <kamil.gabryjelski@gmail.com>
2021-03-07 10:54:08 +0100

    feat: Implement drag and drop columns for filters (#13340)
    * Implement DnD feature for filters
    
    * minor refactor
    
    * Fix types
    
    * Fix undefined error
    
    * Refactor
    
    * Fix ts errors
    
    * Fix conflicting dnd types
    
    * Bump superset-ui packages
    
    * Change DndItemType case to PascalCase
    
    * Remove redundant null check
    
    * Fix
    
    * Fix csrf mock api call

--- superset-frontend/src/explore/components/DndItemType.ts
+++ superset-frontend/src/explore/components/DndItemType.ts
@@ -16,7 +16,25 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-export const OPTION_TYPES = {
-  metric: 'metric',
-  filter: 'filter',
-};
+
+/**
+ * All possible draggable items for the chart controls.
+ */
+export enum DndItemType {
+  // an existing column in table
+  Column = 'column',
+  // a selected column option in ColumnSelectControl
+  ColumnOption = 'columnOption',
+  // an adhoc column option in ColumnSelectControl
+  AdhocColumnOption = 'adhocColumn',
+
+  // a saved metric
+  Metric = 'metric',
+  // a selected saved metric in MetricsControl
+  MetricOption = 'metricOption',
+  // an adhoc metric option in MetricsControl
+  AdhocMetricOption = 'adhocMetric',
+
+  // an adhoc filter option
+  FilterOption = 'filterOption',
+}
