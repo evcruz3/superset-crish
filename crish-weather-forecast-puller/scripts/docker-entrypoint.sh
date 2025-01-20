efcea475f06c51a844c5524431605e75351d5244
evcruz3 <erickson49366@gmail.com>
2025-01-20 18:10:25 +0800

    Update docker-entrypoint.sh

--- crish-weather-forecast-puller/scripts/docker-entrypoint.sh
+++ crish-weather-forecast-puller/scripts/docker-entrypoint.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+set -e
+
+echo "Starting docker-entrypoint.sh"
 
 # Run setup_env.sh to create auth file
+echo "Running setup_env.sh..."
 source scripts/setup_env.sh
+echo "setup_env.sh completed"
+
+# Log the command that will be executed
+echo "Executing command: $@"
 
 # Execute the CMD
 exec "$@"
