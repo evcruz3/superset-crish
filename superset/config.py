d36206cc792139aa37712dad0418378b1ae7a0b5
evcruz3 <erickson49366@gmail.com>
2025-05-12 12:49:25 +0800

    Update config.py

--- superset/config.py
+++ superset/config.py
@@ -311,7 +311,7 @@ AUTH_RATE_LIMIT = "5 per second"
 APP_NAME = "Superset"
 
 # Specify the App icon
-APP_ICON = "/static/assets/images/superset-logo-horiz.png"
+APP_ICON = "/static/assets/images/Logos.png"
 
 # Specify where clicking the logo would take the user'
 # Default value of None will take you to '/superset/welcome'
