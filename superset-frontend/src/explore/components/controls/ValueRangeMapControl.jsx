cf5f4ec98ea31792643c6cd052e5666c091ea580
evcruz3 <erickson49366@gmail.com>
2025-09-03 15:55:57 +0800

    Add chart data zips and fix missing React imports
    Added new weekly case data zip files for ARI, dengue variant, and diarrhea variant to the chart_data directory. Also fixed missing React imports in ValueMappedControl.jsx and ValueRangeMapControl.jsx to resolve potential runtime errors.

--- superset-frontend/src/explore/components/controls/ValueRangeMapControl.jsx
+++ superset-frontend/src/explore/components/controls/ValueRangeMapControl.jsx
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+import React from 'react';
 import PropTypes from 'prop-types';
 import { SketchPicker } from 'react-color';
 import { debounce } from 'lodash';
